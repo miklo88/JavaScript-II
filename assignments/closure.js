1ecd14da954887626c7479a45ca59501de9272ed
Carlos <miklo.concepcion@gmail.com>
2019-07-09 17:41:30 -0500

    need to review closure for its job application importance. Higher Function and Array Methods need to complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,21 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+//example 1
+const foo = 'bar';
+function returnFoo () {
+  return foo;
+}
+returnFoo();
+
+//example 2
+const lastName = 'Bond';
+function greet() {
+  const firstName = 'James';
+  alert(`The name's ${lastName}, ${firstName} ${lastName}`)
+}
+
+console.log(lastName);
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,10 +23,19 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+  let count = 0;
+  return function() {
+    // count = count + 1
+    return ++count;
+  }
+};
+const newCounter = counter();
+console.log(newCounter());
+// console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
